0caf2144f9b4632af4bb3287c06030ec4713c006
Wang Zhuochun <stone1551@gmail.com>
2012-07-22 22:48:31 +0800

    add module format converter in helper.js
    add module format converter in helper.js and test cases for helper.js

--- src/js/utils/helper.js
+++ src/js/utils/helper.js
@@ -31,4 +31,40 @@ define(function(require, exports) {
         };
     };
 
+    // convert module lectures/modules/labs sequences
+    exports.convertModule = function(mod) {
+        var m = $.extend({}, mod); // duplicate
+
+        if (!$.isEmptyObject(m.lectures)) {
+            m.hasLecture = true;
+            m.lectures = convertClass(m.lectures);
+        }
+
+        if (!$.isEmptyObject(m.tutorials)) {
+            m.hasTutorial = true;
+            m.tutorials = convertClass(m.tutorials);
+        }
+
+        if (!$.isEmptyObject(m.labs)) {
+            m.hasLab = true;
+            m.labs = convertClass(m.labs);
+        }
+
+        return m;
+    };
+
+    function convertClass(klass) {
+        var i, j = 0, length, key, result = [];
+
+        for (key in klass) {
+            length = klass[key].length;
+
+            for (i = 0; i < length; i++) {
+                result[j++] = klass[key][i];
+            }
+        }
+
+        return result;
+    }
+
 });
---
+++ test/spec/utilTest.js
@@ -0,0 +1,45 @@
+/* ========================================
+ * CORS Planner - Utils Tests
+ *
+ * Author: Wang Zhuochun
+ * Last Edit: 22/Jul/2012 07:59 PM
+ * ========================================
+ * <License>
+ * ======================================== */
+
+define(function(require, exports) {
+
+    "use strict";
+    /*jshint browser:true, jquery:true, laxcomma:true, maxerr:50*/
+    /*globals describe, xdescribe, it, xit, expect, beforeEach, afterEach*/
+
+/* Test Helper Util
+ * ======================================== */
+    describe("Util/Helper", function() {
+
+        var helper = require("util/helper")
+        , acc1002 = require("json!testData/acc1002parsed.json")
+        , cs1020 = require("json!testData/cs1020parsed.json");
+
+        it("can get the semester accordingly", function() {
+            // Change it manually as time goes
+            var current = "2012/2013", sem = 1
+            , fromHelper = helper.getSemester();
+
+            expect(fromHelper.acadYear).toEqual(current);
+            expect(fromHelper.semester).toEqual(sem);
+        });
+
+        it("can convert module between formats", function() {
+            var converted = helper.convertModule(acc1002);
+            
+            expect(converted.code).toBe("ACC1002");
+            expect(converted.hasLecture).toBe(true);
+            expect(converted.lectures.length).toEqual(1);
+            expect(converted.hasTutorial).toBe(true);
+            expect(converted.tutorials.length).toEqual(13);
+        });
+
+    });
+
+});

2556b54df4231e1d9493320b3e730e0f1837ed3e
Wang Zhuochun <stone1551@gmail.com>
2012-07-29 02:07:28 +0800

    update app to include planner global
    update app to include planner global

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,8 +1,10 @@
 /* ========================================
  * CORS Planner - App Main
+ * 
+ * Load all app components required
  *
  * Author: Wang Zhuochun
- * Last Edit: 23/Jul/2012 09:33 PM
+ * Last Edit: 28/Jul/2012 10:41 PM
  * ========================================
  * <License>
  * ======================================== */
@@ -11,6 +13,7 @@ define(function(require, exports) {
 
     "use strict";
     /*jshint jquery:true, laxcomma:true, maxerr:50*/
+    /*globals planner*/
 
     // include jQuery plugins
     require("jquery-ui");
@@ -18,33 +21,51 @@ define(function(require, exports) {
     // include the jQuery Pub/Sub plugin
     require("util/pubsub");
 
+    // include third party
+    var store = require("util/store")
+    // include controllers
+    , modulesController = require("controller/modulesController")
+    , plannerController = require("controller/plannerController")
     // include app view
-    var version = "0.0.5"
-    // include components
-      , store = require("util/store")
-      , appView = require("view/appView");
+    , appView = require("view/appView");
+
+
+    // expose a planner variable in global namespace
+    // 
+    // Taken from: (in case window is not the point to the global object)
+    //   http://stackoverflow.com/questions/3277182/how-to-get-the-global-object-in-javascript
+    function _initGlobalPlanner() {
+        var Fn = Function, global = (new Fn('return this'))();
+
+        if (!global.planner) {
+            global.planner = {};
+        }
+
+        // assign CORS Planner Version number
+        planner.version = "0.0.6";
+        // assign CORS Data latest update time
+        planner.dataUpdate = "24 Jul 2012 11:29";
+    }
+
+    // check the version between server and user, emits an message to all
+    function _versionCheck(_version) {
+        // get user's version
+        _version = store.get("version");
+        // set version again
+        store.set("version", planner.version);
+        // publish the app wide message related to user
+        $.publish("app:user:" + ((!_version) ? "new" :
+            (_version < planner.version) ? "updated" : "uptodate"));
+    }
 
     // exports module
     exports.init = function(v) {
-        // send greeting messages
-        greetUser();
-        // initial app views
+        // init app itself
+        _initGlobalPlanner();
+        // render all the app views
         appView.render();
+        // version check
+        _versionCheck();
     };
-
-    // greeting user according to version
-    function greetUser(_version) {
-        _version = store.get("version");
-
-        if (!_version) { // new user
-            $.publish("app:newUser", [version]);
-        } else if (_version < version) { // on older version
-            $.publish("app:update", [version]);
-        } else { // existing user
-            $.publish("app:intro", [version]);
-        }
-
-        store.set("version", version); // set version
-    }
 
 });

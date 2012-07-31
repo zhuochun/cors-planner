303b12962b56242201b5f690f98811179827226d
Wang Zhuochun <stone1551@gmail.com>
2012-08-01 02:24:38 +0800

    update plannerView
    separate plannerView into smaller timetableView

--- src/js/app/views/plannerView.js
+++ src/js/app/views/plannerView.js
@@ -11,114 +11,24 @@ define(function(require, exports) {
 
     "use strict";
     /*jshint browser:true, jquery:true, laxcomma:true, maxerr:50*/
+    /*global planner*/
 
-    var $el = $("#timetable")
-    , $grid = $("#table-grid"), $gridTr, $gridTd, $gridWidth
-    , $slot = $("#table-slot")
-    // local variables
-    , weekDays = { "MONDAY" : 0, "TUESDAY" : 1, "WEDNESDAY" : 2, "THURSDAY" : 3, "FRIDAY": 4 }
-    , types = { "lectures" : "(L)", "tutorials" : "(T)", "labs" : "(Lab)" };
+	// get components
+    var timetableView = require("view/timetableView");
 
     // initial timetable grids
     exports.init = function() {
-        initTimetable();
+        timetableView.init(planner.timetable);
     };
 
-    // create droppable slots
-    exports.createDroppableSlot = function(code, type, klass) {
-        var key, info, top, left, width, $div; 
-      //var $width = $tbody[0].offsetWidth;
-        // cache $grid related DOM if not defined
-        $gridTr = $gridTr || $grid.find("tbody tr");
-        $gridTd = $gridTd || $gridTr.find("td");
-        $gridWidth = $gridWidth || $gridTr[0].offsetWidth;
-        // detach $slot to append slot
-        $slot.detach();
+    // subscribe to start/end time change, re-render table
+    $.subscribe("timetable:range", function(e, start, end) {
+		timetableView.render(planner.timetable, start, end);
+    });
 
-        // allocate klass to $slot
-        for (key in klass) {
-            info = key.split("-"); // key is in format: "MONDAY-0900-1000"
-            // set top, according to its weekDay
-            top = $gridTr[weekDays[info[0]]].offsetTop + 2;
-            // set left and width, according to start and end time
-            left = $gridTd[getTimeIndex(info[1])].offsetLeft + 1;
-            width = $gridTd[getTimeIndex(info[2])].offsetLeft - left;
-            // convert left and width to percentage
-            left = (left / $gridWidth) * 100 + "%";
-            width = (width / $gridWidth) * 100 + "%";
-            // create the div to hold this slot
-            $div = $("<div>").addClass("bar temp-slot " + type)
-                .text(code + " " + types[type])
-                .css({"top" : top, "left" : left, "width" : width})
-                .droppable(dropOpts);
-            // append div to slot
-            $div.appendTo($slot);
-        }
-        // put $slot back
-        $slot.appendTo($el);
-    };
-
-    // TODO: refactoring
-    var dropOpts = {
-        activeClass: "bar-active"
-        , hoverClass: "bar-hover"
-        , drop: function(event, ui) {
-            ui.draggable.removeClass("draggable");
-           // window.setTimeout(function() {
-           //     ui.draggable("destroy");
-           // }, 1000);
-            $(this).css("background-color", "grey").removeClass("temp-slot");
-            //$(this).draggable({
-            //    cursor: "move"
-            //    , opacity: 0.6
-            //    , revert: "invalid"
-            //    //, start: dragEvent(module1)
-            //    , stop: function() {
-            //        $("#events").find(".temp-slot").remove();
-            //    }
-            //});
-            $slot.find(".temp-slot").remove();
-        }
-    };
-
-    function getTimeIndex(t) {
-        t = parseInt(t, 10);
-        return (Math.floor(t / 100) - 8) * 2 + 1 + (t % 100 ? 1 : 0) ;
-    }
-
-    // generate a timetable
-    function initTimetable(startHour, endHour) {
-        // set default variables
-        startHour = startHour || 8; // startHour default = 8AM
-        endHour = endHour || 21; // endHour default = 21PM
-
-        // set table variables
-        var i, j, k, thead = [], tbody = []
-        , table = ["<table class='table table-striped table-bordered'>"];
-
-        // generate thead
-        for (i = startHour, j = 0; i <= endHour; i++, j++) {
-            thead[j] = "<th colspan='2'>" + (i < 10 ? "0" + i : i) + "</th>";
-        }
-        // push thead to table
-        table.push("<thead><tr><th></th>" + thead.join("") + "</tr></thead>");
-
-        // generate tbody
-        j = 0;
-        for (i in weekDays) {
-            tbody[j] = "<tr><td class='weekday'>" + i + "</td>";
-
-            for (k = startHour; k <= endHour; k++)
-                tbody[j] += "<td></td><td></td>";
-
-            tbody[j] += "</tr>";
-            j++;
-        }
-        // push tbody to table
-        table.push("<tbody>" + tbody.join(" ") + "</tbody");
-
-        // append table to html
-        $grid.html(table.join(""));
-    }
+	// subscribe to timetable orientation change
+	$.subscribe("timetable:type", function(e, type) {
+		timetableView.render(type);
+	});
 
 });

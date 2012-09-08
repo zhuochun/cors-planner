f8d20ea0bd9432c7c5a1aaacf6e1bcbfa0ee5c84
Mifeng <stone1551@gmail.com>
2012-09-08 22:16:51 +0800

    update messageView to use message pop up
    update message view to display information using toastr plugin

--- src/js/app/views/messageView.js
+++ src/js/app/views/messageView.js
@@ -11,17 +11,33 @@ define(function(require, exports) {
 
     "use strict";
     /*jshint browser:true, jquery:true, laxcomma:true, maxerr:50*/
-    /*global window*/
+    /*global planner*/
+    
+    var toastr = require("util/toastr");
 
     // render initial all App Views
     exports.init = function() { };
 
-    $.subscribe("message:alert", function(e, message) {
-        window.alert("ALERT: " + message);
+    $.subscribe("message:error", function(e, message) {
+        toastr.error(message);
     });
 
     $.subscribe("message:info", function(e, message) {
-        window.alert("NOTICE: " + message);
+        toastr.info(message);
+    });
+
+    $.subscribe("message:warning", function(e , message) {
+        toastr.warning(message);
+    });
+
+    $.subscribe("message:success", function(e, message) {
+        toastr.success(message);
+    });
+
+    $.subscribe(planner.list.modules + ":duplicatedExamDate", function(e, m1, m2) {
+        toastr.warning("Exam Date (" + m1.get("examDate") + ") clashes between " +
+            m2.get("code") + " " + m2.get("title") + " and " +
+            m1.get("code") + " " + m1.get("title") + ".");
     });
 
 });

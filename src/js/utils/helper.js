6f5417be36c280d80696ec4d3325d6a80207aa9f
Wang Zhuochun <stone1551@gmail.com>
2013-06-29 22:36:08 +0800

    temp fix for ntu time

--- src/js/utils/helper.js
+++ src/js/utils/helper.js
@@ -21,10 +21,12 @@ define(function(require, exports) {
           , year = today.getFullYear()
           , month = today.getMonth(); // month is [0, 11]
 
-        // Jan 0 - Jun 5 (this year sem 2)
-        // Jul 6 - Nov 10 (next year sem 1)
+        // TODO: change this school's specific folder
+
+        // Jan 0 - May 4 (this year sem 2)
+        // Jul 5 - Nov 10 (next year sem 1)
         // Dec 11 (next year sem 2)
-        if (month <= 5) {
+        if (month <= 4) {
             result.startYear = year - 1;
             result.endYear = year;
             result.semester = 2;

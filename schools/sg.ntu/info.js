b227f32cedcc71107454f926cb6345cb59f61cb1
zhuochun <stone1551@gmail.com>
2013-07-26 12:49:40 +0800

    version 0.7.4

--- schools/sg.ntu/info.js
+++ schools/sg.ntu/info.js
@@ -1,1 +1,1 @@
-define(["require","exports","module"],function(e,t){return{name:"Nanyang Technological University",abbr:"NTU",url:"http://www.ntu.edu.sg/",lastUpdate:"Thu Jul 04 2013 00:22:57 GMT+0800 (Malay Peninsula Standard Time)"}});
+define(["require","exports","module"],function(e,t){return{name:"Nanyang Technological University",abbr:"NTU",url:"http://www.ntu.edu.sg/",modules:0,lastUpdate:"Thu Jul 04 2013 00:22:57 GMT+0800 (Malay Peninsula Standard Time)"}});
--- schools/sg.nus/info.js
+++ schools/sg.nus/info.js
@@ -1,1 +1,1 @@
-define(["require","exports","module"],function(e,t){return{name:"National University of Singapore",abbr:"NUS",url:"http://www.nus.edu.sg/",lastUpdate:"Mon Jul 15 2013 21:47:33 GMT+0800 (Malay Peninsula Standard Time)"}});
+define(["require","exports","module"],function(e,t){return{name:"National University of Singapore",abbr:"NUS",url:"http://www.nus.edu.sg/",modules:1787,lastUpdate:"Fri Jul 26 2013 12:46:12 GMT+0800 (Malay Peninsula Standard Time)"}});

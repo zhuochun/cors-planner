1b8a4491acd23fa1e65c05711a35a25b7eb8c3ed
jin <jin@crypt.sg>
2013-08-09 20:29:16 +0800

    Fixed indentation

--- makefile.sh
+++ makefile.sh
@@ -1,22 +1,22 @@
 #Building
 echo "Start Building CORS-PLANNER" 
 
-		echo "removing release folder"
-				#remove release
-				rm -rf release/
+  echo "removing release folder"
+    #remove release
+    rm -rf release/
 
-		echo "calling r.j to build using build.js"
-				#build javascripts
-				node build/r.js -o build/build.js
+  echo "calling r.j to build using build.js"
+    #build javascripts
+    node build/r.js -o build/build.js
 
-		echo "modifying html script includes"
-				#modify index.html to use css
-				node build/clean.html.js
+  echo "modifying html script includes"
+    #modify index.html to use css
+    node build/clean.html.js
 
-		echo "compiling less to css"
-				#create folder in release for css files
-				mkdir -p release/css
-				#build less to css file
-				lessc --yui-compress src/less/style.less > release\css\style.css
+  echo "compiling less to css"
+    #create folder in release for css files
+    mkdir -p release/css
+    #build less to css file
+    lessc --yui-compress src/less/style.less > release\css\style.css
 
 echo "Build Succeed"

c05e66f542663c58a0a25ca9cc0162a2220222d0
Mifeng <stone1551@gmail.com>
2014-01-02 23:27:22 +0800

    update with vertical layout (experimental)

--- schools/sg.nus/info.js
+++ schools/sg.nus/info.js
@@ -1,1 +1,1 @@
-define(["require","exports","module"],function(e,t){return{name:"National University of Singapore",abbr:"NUS",url:"http://www.nus.edu.sg/",modules:1787,lastUpdate:"Fri Jul 26 2013 12:46:12 GMT+0800 (Malay Peninsula Standard Time)"}});
+define(["require","exports","module"],function(e,t){return{name:"National University of Singapore",abbr:"NUS",url:"http://www.nus.edu.sg/",modules:1846,lastUpdate:"Thu Jan 02 2014 22:35:35 GMT+0800 (Malay Peninsula Standard Time)"}});

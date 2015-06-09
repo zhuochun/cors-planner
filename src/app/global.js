d60e5b0b0c715cd62f7523681aa4ee82576455e1
Wang Zhuochun <zhuochun@hotmail.com>
2015-06-10 01:22:26 +0800

    upgrade to v0.8.0

--- src/app/global.js
+++ src/app/global.js
@@ -30,7 +30,7 @@ define(function(require, exports) {
         }
 
         // assign CORS Planner Version number
-        planner.version = "0.7.10";
+        planner.version = "0.8.0";
         // CORS Planner for schools
         planner.school = store.get("app:school") || null;
         // default module lists
